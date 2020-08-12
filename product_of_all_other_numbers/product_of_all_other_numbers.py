6c1dcc4d916e1d9bea16ab63d0f95def98954656
Cody-Hayes97 <hayesmichael2@gmail.com>
2020-08-11 20:00:20 -0700

    mvp

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -3,9 +3,33 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def product_of_all_other_numbers(arr):
-    # Your code here
+#    set the current index and create new array
+   curr_i = 0
+   result = []
+#    make a loop that ends when the current index is equal to the length of the arr
+   while curr_i < len(arr):
+    #    set a new pointer for the index of the inside while loop
+       inside = 0
+    #    set the base that each index will be multiplied by 
+       new_res = 1
+    #    make a loop for the inside index so that it ends when the inside pointer is equal to the length of the arr
+       while inside < len(arr):
+        #    if the two pointers are equal, move index to point to the number after
+           if inside == curr_i:
+               inside += 1
+           else:
+            #    set the base num to be itself multiplied by the index of the array based on the inside pointer
+               new_res = new_res * arr[inside]
+            #    incrememnt the inside pointer
+               inside += 1
+        # append the multiplication of each inside index to the new array
+       result.append(new_res)
+    #    incrememnt the current pointer and the nested while loop starts again
+       curr_i += 1
+   return result
+    
 
-    pass
+
 
 
 if __name__ == '__main__':
